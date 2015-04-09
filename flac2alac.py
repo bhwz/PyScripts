9494d7c6a44e61534cfd0d311a858c2ee656b4ab
Calcium <calcium@calcium.pw>
2015-04-09 02:47:17 -0700

    Fix flac2alac putting an extra full stop in output file extensions

--- flac2alac.py
+++ flac2alac.py
@@ -4,7 +4,7 @@ from subprocess import call
 for flcFile in listdir(getcwd()):
     if flcFile.endswith(".flac"):
         print(flcFile)
-        flcName = flcFile[:-4]
+        flcName = flcFile[:-5]
         ffmpegArgs = " -i \"{0}\" -acodec alac \"{1}\".m4a".format(flcFile, flcName)
         call("ffmpeg" + ffmpegArgs, shell=True)
         continue
